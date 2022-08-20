14c779505577f072efd4d5c5571a1df85d4c0d30
Juan Augusto <83130876+Juan-Augusto@users.noreply.github.com>
2022-08-20 10:23:18 -0300

    Questão 4

--- src/Main.java
+++ src/Main.java
@@ -5,6 +5,7 @@ public class Main {
         moto Moto = new moto("honda MXRT", 2020, "verde", 25000, 3, "esportiva");
         caminhao Caminhao = new caminhao("caminhao MXRT", 2021, "preto", 200000, 5, 3);
         onibus Onibus = new onibus("onibus padrao", 2018, "verde", 220000, 6, 30);
+        seguro veiculoEscolhido = new seguro(1, "carro");
         System.out.println(Carro + Carro.getTipo());
         System.out.println(Moto + Moto.getTipo());
         System.out.println(Caminhao + "" + Caminhao.getCapacidade());
@@ -13,11 +14,7 @@ public class Main {
         }else{
             System.out.println("Preco de venda do carro = " + Carro.getPrecoCompra() * 1.15);
         }
-        if(Objects.equals(Carro.getCor(), "esportiva")
-                &&
-                (Objects.equals(Carro.getCor(), "verde")
-                        ||
-                        Objects.equals(Carro.getCor(), "vermelho"))){
+        if(Objects.equals(Carro.getCor(), "esportiva") && (Objects.equals(Carro.getCor(), "verde") ||Objects.equals(Carro.getCor(), "vermelho"))){
             System.out.println("Preco de venda da moto = " + (Moto.getPrecoCompra() * 1.4));
         }else{
             System.out.println("Preco de venda da moto = " + (Moto.getPrecoCompra() * 1.1));
@@ -27,11 +24,29 @@ public class Main {
         }else{
             System.out.println("Preco de venda do caminhao = " + (Onibus.getPrecoCompra() * 1.05));
         }
-        if( Onibus.getQtdAssentos() > 15 && Onibus.getAnoFabricacao() > 2020 ){
+        if(Onibus.getQtdAssentos() > 15 && Onibus.getAnoFabricacao() > 2020 ){
             System.out.println("Preco de venda do onibus = " + (Onibus.getPrecoCompra() * 1.12));
         }else{
             System.out.println("Preco de venda do onibus = " + (Onibus.getPrecoCompra() * 1.08));
         }
-
+        if(Objects.equals(veiculoEscolhido.getVeiculo(), "carro")){
+            if(veiculoEscolhido.getTipo() == 2){
+                System.out.println("Preco do seguro do carro: " + (Carro.getPrecoCompra() * 0.03));
+            }else{
+                System.out.println("Preco do seguro do carro: " + (Carro.getPrecoCompra() * 0.02));
+            }
+        }else if(Objects.equals(veiculoEscolhido.getVeiculo(), "caminhao")){
+            if(veiculoEscolhido.getTipo() == 2){
+                System.out.println("Preco do seguro do carro: " + (Caminhao.getPrecoCompra() * 0.03));
+            }else{
+                System.out.println("Preco do seguro do carro: " + (Caminhao.getPrecoCompra() * 0.02));
+            }
+        }else{
+            if(Objects.equals(veiculoEscolhido.getVeiculo(), "moto")){
+                System.out.println("Preco do seguro da moto: " + (Moto.getPrecoCompra() * 0.02));
+            }else{
+                System.out.println("Preco do seguro do onibus: " + (Onibus.getPrecoCompra() * 0.02));
+            }
+        }
     }
 }
